b0cee697e71ddc663e76b0ee9012356ce16012f5
Filipi Martins <filipi.martinss@hotmail.com>
2025-01-13 18:17:34 -0300

    adiciona `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  response.status(200).json({ valor: "Deu certo àgua" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  console.log((await database.query("SELECT 1 + 1 AS sum;")).rows);
+  response.status(200).json({ valor: "Deu certo água" });
 }
 
 export default status;
